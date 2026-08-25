d0cb6ca605dbfe7dd4ce0a511250e36d1896fa7e
Ionut Constantin Majoru <ionutmajoru@gmail.com>
2026-08-25 16:52:49 +0200

    0.0.2

--- cerveceria.py
+++ cerveceria.py
@@ -1,4 +1,8 @@
-print('El nombre de tu cerveceria sera:\n'
-      + input('Que animal o elemento de la naturaleza representa el caracter de la marca?: ')
-      + " "
-      + input('Que adjetivo define su sabor o potencia?: ') + '\nFelicidades!!')
+#Cerveceria
+#print('El nombre de tu cerveceria sera:\n'
+#      + input('Que animal o elemento de la naturaleza representa el caracter de la marca?: ')
+#      + " "
+#      + input('Que adjetivo define su sabor o potencia?: ') + '\nFelicidades!!')
+
+#Comisiones
+print("")
